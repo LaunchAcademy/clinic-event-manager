a9a9ca07cd0479a362ea586a72d5036bb408d767
Kerrin Gillis <kerrin.gillis@launchacademy.com>
2022-10-05 17:09:41 -0400

    finished core steps

---
+++ src/components/EventTile.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+const EventTile = (props) => {
+  // debugger
+  let eventClass = ""
+  if (props.eventType === "personal") {
+    eventClass = "green-bg"
+  } else if (props.eventType === "business") {
+    eventClass = "blue-bg"
+  } else if (props.eventType === "business-casual") {
+    eventClass = "pink-bg"
+  }
+
+  return (
+    <div className={eventClass}>
+      <h3>{props.title}</h3>
+      <p>{props.timePeriod}</p>
+      <p>{props.description}</p>
+    </div>
+  )
+}
+
+export default EventTile
